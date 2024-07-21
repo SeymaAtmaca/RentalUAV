f3e0a0ab2494e00c6e00edd60e4868faf36553e3
Şeyma Atmaca <54944249+SeymaAtmaca@users.noreply.github.com>
2024-07-21 21:46:10 +0300

    Update settings.py

--- UAV_Rental/uavrental/uavrental/settings.py
+++ UAV_Rental/uavrental/uavrental/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-ep@jpe(sj%+bmomim^dt38^^8%hicof48)vj01sl3v@+*)ld24'
+SECRET_KEY = '......'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
